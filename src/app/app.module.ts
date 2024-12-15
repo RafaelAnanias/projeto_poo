fad3cf181226a526fa27dd92cea1277f0bcfb607
Rafael Ananias <rafaelananias33@gmail.com>
2024-12-15 01:15:22 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { GameListComponent } from './game-list/game-list.component';
+import { AddGameComponent } from './add-game/add-game.component';
+import { GameDetailsComponent } from './game-details/game-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GameListComponent,
+    AddGameComponent,
+    GameDetailsComponent
   ],
   imports: [
     BrowserModule,
